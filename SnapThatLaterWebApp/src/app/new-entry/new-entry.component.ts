99cb942efd2593a4d9c4be067bb03c1e4bdaaa53
Will Andrews <Will.Andrews@civica.co.uk>
2018-05-09 13:09:41 +0100

    Added in a google maps locater to get the users current location

--- SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
+++ SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-new-entry',
@@ -8,10 +8,48 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class NewEntryComponent implements OnInit {
 
   @Output() onSaved = new EventEmitter<boolean>();
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  marker: google.maps.Marker;
   
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+  onFindMe() : void {
+    console.log('trying to find you');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showMe(position);
+      });
+    } else {
+      alert("Geolocation not available for this browser");
+    } 
+  }
+
+  showMe(position) : void {
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+
   }
 
   onSaveClicked() : void {
