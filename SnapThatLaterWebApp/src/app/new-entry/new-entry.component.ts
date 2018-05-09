1df2243af843c9c7e722abecf3afd9efb3471651
Will Andrews <Will.Andrews@civica.co.uk>
2018-05-09 15:18:21 +0100

    Tidying up the camera and map elements

--- SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
+++ SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
@@ -8,24 +8,23 @@ import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/cor
 export class NewEntryComponent implements OnInit {
 
   @Output() onSaved = new EventEmitter<boolean>();
-  @ViewChild('gmap') gmapElement: any;
+  @ViewChild('googlemap') gmapElement: any;
+  
   map: google.maps.Map;
   marker: google.maps.Marker;
   
   constructor() { }
 
   ngOnInit() {
-    var mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
+    var mapOptions = {
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapOptions);
   }
 
   onFindMe() : void {
-    console.log('trying to find you');
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.showMe(position);
@@ -36,20 +35,16 @@ export class NewEntryComponent implements OnInit {
   }
 
   showMe(position) : void {
+    
     let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    this.map.panTo(location);
+   
+    this.map.setCenter(location);
 
-    if (!this.marker) {
-      this.marker = new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: 'Got you!'
-      });
-    }
-    else {
-      this.marker.setPosition(location);
-    }
-
+    this.marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: 'This is your current location'
+    });
   }
 
   onSaveClicked() : void {
