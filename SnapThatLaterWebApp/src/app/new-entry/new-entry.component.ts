6e182af2e0c2c6d6d6f97c5f890761c425a265c6
willdot <will7989@hotmail.com>
2018-05-13 13:48:25 +0100

    Updated packages. Display location

--- SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
+++ SnapThatLaterWebApp/src/app/new-entry/new-entry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { loadavg } from 'os';
+//import { loadavg } from 'os';
 import { GoogleLocationService } from '../services/google-location.service';
 
 
